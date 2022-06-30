6bbad61020f030fc97b29ca1b5ed604f4967e0e9
Erika Hamilton <hamiltone@g.cofc.edu>
2022-06-30 11:44:07 -0400

    Fixed Transient Error/ no relaitons

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,81 +1,69 @@
 package guru.springframework.spring5webapp.bootstrap;
+
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
-//import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRespository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
-//import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * Created by jt on 12/23/19.
+ */
 @Component
 public class BootStrapData implements CommandLineRunner {
-    private final AuthorRespository authorRespository;
-    private final BookRepository bookRepository;
-   // private final PublisherRepository publisherRepository;
-    
-  
-    public BootStrapData(AuthorRespository authorRespository, BookRepository bookRepository) {
-        this.authorRespository = authorRespository;
-        this.bookRepository = bookRepository;
-      
-    }
 
+    private final AuthorRespository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRespository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-        // TODO Auto-generated method stub
-        System.out.println("Started in BootStrap.");
-        // Publisher publisher = new Publisher();
-        // publisher.setPubName("SFG Petersburg");
-        // publisher.setCity("St. Petersberg");
-        // publisher.setState("FL");
-        // publisher.setAddress("add1");
-        // publisher.setZip("zip");
 
-        // publisherRepository.save(publisher);
-        //  System.out.println("Publisher Count: "+ publisherRepository.count());
-    
-        
-        Author eric = new Author("Eric", "Evans" );
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+        Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
-       // ddd.setPublisher(publisher);
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
 
-       // publisher.getBooks().add(ddd);
-
-      
-       
-
-        authorRespository.save(eric);
+        authorRepository.save(eric);
         bookRepository.save(ddd);
-       // publisherRepository.save(publisher);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "39281031");
-
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
-        //noEJB.setPublisher(publisher);
-       // publisher.getBooks().add(noEJB);
 
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
 
-
-        authorRespository.save(rod);
+        authorRepository.save(rod);
         bookRepository.save(noEJB);
-        //publisherRepository.save(publisher);
+        publisherRepository.save(publisher);
 
-       
         System.out.println("Number of Books: " + bookRepository.count());
-       
-       // System.out.println("No of Books to Publisher: " + publisher.getBooks().size());
-
-
-        
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
     }
-
-
-    
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Book {
@@ -19,6 +20,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+
+    @ManyToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -89,6 +93,13 @@ public boolean equals(Object obj) {
         return false;
     return true;
 }
+public void setPublisher(Publisher publisher) {
+    this.publisher = publisher;
+}
+public Publisher getPublisher() {
+    return publisher;
+}
+
 
     
 }
